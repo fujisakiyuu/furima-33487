6e6bb7e8531fd5a298f05dc6b4dfc7c63a748e9f
fujisakiyuu <yurayurataro@icloud.com>
2021-01-29 23:10:04 +0900

    ActiveHashのアソシエーション作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,10 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to :category
+  belongs_to :state
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :day
+
 end
