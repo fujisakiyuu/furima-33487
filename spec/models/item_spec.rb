d40c25f88eea275b68e9e7a1a2cccf41ee99f987
fujisakiyuu <yurayurataro@icloud.com>
2021-01-31 18:35:51 +0900

    修正１

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,6 +6,11 @@ RSpec.describe Item, type: :model do
       @item = FactoryBot.build(:item)
       @item.image = fixture_file_upload('public/images/sample1.png')
     end
+    context '商品出品できるとき' do
+      it '全ての値が正しく入力されていれば保存できる' do
+        expect(@item).to be_valid
+      end
+    end
     
     context '商品出品できないとき' do
       it '商品名が空だと登録できない' do
@@ -23,25 +28,50 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Category can't be blank"
       end
+      it 'カテゴリーが--だと登録できない' do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Category must be other than 1"
+      end
       it '商品の状態が空だと登録できない' do
         @item.state_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include "State can't be blank"
+      end
+      it '商品の状態が--だと登録できない' do
+        @item.state_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "State must be other than 1"
       end
       it '発送料の負担が空だと登録できない' do
         @item.burden_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include "Burden can't be blank"
       end
+      it '発送料の負担が--だと登録できない' do
+        @item.burden_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Burden must be other than 1"
+      end
       it '発送元の地域が空だと登録できない' do
         @item.area_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include "Area can't be blank"
       end
+      it '発送元の地域が--だと登録できない' do
+        @item.area_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Area must be other than 1"
+      end
       it '発送までの日数が空だと登録できない' do
         @item.day_id = ""
         @item.valid?
         expect(@item.errors.full_messages).to include "Day can't be blank"
+      end
+      it '発送までの日数が--だと登録できない' do
+        @item.day_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Day must be other than 1"
       end
       it '価格が空だと登録できない' do
         @item.price = ""
@@ -58,12 +88,26 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Price Half-width number"
       end
-      it '価格の範囲が¥300~¥9999999以外だと登録できない' do
+      it '価格の範囲が¥300以下だと登録できない' do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not included in the list"
       end
-
+      it '価格の範囲が¥9999999以上だと登録できない' do
+        @item.price = '100000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not included in the list"
+      end
+      it '価格が半角英数混合だと登録できない' do
+        @item.price = 'aa11'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price Half-width number"
+      end
+      it '価格が半角英語だと登録できない' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price Half-width number"
+      end
 
   end
  end
