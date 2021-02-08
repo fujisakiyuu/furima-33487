b9b7d9d607c978eb8219921b5198e1db25fa0b26
fujisakiyuu <yurayurataro@icloud.com>
2021-02-08 11:53:49 +0900

    formオブジェクト、クレジットカード決済

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,33 +1,62 @@
 class OrdersController < ApplicationController
-  before_action :item_user, only: [:new]
+  before_action :item_user, only: [:index, :create]
   def index
-  end
-
-  def new
     @user_order = UserOrder.new
   end
 
-  def create
-    @user_order = UserOrder.new(order_params)   
-     if @user_order.valid?
-       @user_order.save
-       redirect_to action: :index
-     else
-       render action: :new
-     end
+  def new
+    @user_order = UserOrder.new   
   end
 
 
   
+  
+  def create
+    @user_order = UserOrder.new(user_order_params)   
+     if @user_order.valid?
+        pay_item
+       @user_order.save
+       redirect_to root_path
+     else
+      render :index
+     end
+    end
+
+  
   private
 
- def order_params
-  params.require(:user_order).permit(:postal_code, :area_id, :municipality, :address, :building, :phone_number)
- end
+  def user_order_params
+   params.require(:user_order).permit(:postal_code, :area_id, :municipality, :address, :building, :phone_number).merge(token: params[:token],item_id: params[:item_id], user_id: current_user.id)
+  end
+
+  # def order_params 
+  #   #  params.permit(:price).merge(item_id: params[:item_id], user_id: current_user.id)
+  #   params.permit(:item_id).merge(user_id: current_user.id)
+  # end
+
+
+  # # paramsからitem_idをとる => params.permit(:item_id)
+  # # user_idはparamsに入っていない => mergeで追加する。
+
+  # def address_params(order)
+  #   params.require(:user_order).permit(:postal_code, :area_id, :municipality, :address, :building, :phone_number).merge(order_id: order.id )
+  # end
+
+
+
 
   def item_user
     @item = Item.find(params[:item_id])
   end
   
+  def pay_item
+ 
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+     Payjp::Charge.create(
+       amount: @item.price,  
+      card: user_order_params[:token],    
+      currency: 'jpy'                 
+    )
+  end
 end
 
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,20 +1,28 @@
 class UserOrder
   include ActiveModel::Model
-  attr_accessor  :postal_code, :area_id, :municipality, :address, :building, :phone_number, :buy
+  attr_accessor  :postal_code, :area_id, :municipality, :address, :building, :phone_number, :order_id, :token, :item_id, :user_id
 
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)" }
     validates :area_id
     validates :municipality
     validates :address
-    validates :building
     validates :phone_number
-    validates :buy
+    validates :token
   end
 
+  
+
   def save
-     Address.create(postal_code: postal_code, area_id: area_id, municipality: municipality, address: address, building: building, phone_number: phone_number, user_id: user.id)
-     Order.create(item_id: item_id, user_id: user_id)
+    order = Order.create(item_id: item_id, user_id: user_id)
+     Address.create(
+       postal_code: postal_code, 
+       area_id: area_id, 
+       municipality: municipality, 
+       address: address, 
+       building: building, 
+       phone_number: phone_number, 
+       order_id: order.id)
   end
 end
 
